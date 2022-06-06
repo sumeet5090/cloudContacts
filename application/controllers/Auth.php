5f9da764a685fa2b0b0719366bbec4a760ed1f41
sumeet5090 <58212406+sumeet5090@users.noreply.github.com>
2022-06-06 08:28:17 +0530

    6th june late night

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -14,8 +14,14 @@ class Auth extends CI_Controller {
 
 	public function login(){
 
+        // lets check if the user has the session enabled
+        // if yes then we will redirect the request to home page
+        // else we will remove the previous session of the user
+        is_logged_in();
 
+        unset($_SESSION['current_user']);
 
+        
 		$this->load->view('head', ['title' => 'Login']);
         $this->load->view('header');
         $this->load->view('login');
--- application/helpers/functions.php
+++ application/helpers/functions.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Users extends CI_Model {
+function is_logged_in(){
+    
 
-    private $table = 'users';
 }
